2cc87d91e275750a4bb3cc5b7a91e2e5854ed16b
Shamith <shamith301102@gmail.com>
2023-02-27 00:13:13 +0530

    Update Number Crunching_Generate Circular Prime.py

--- CTAT_EIS_TEST_2/Number Crunching_Generate Circular Prime.py
+++ CTAT_EIS_TEST_2/Number Crunching_Generate Circular Prime.py
@@ -62,3 +62,31 @@ for n in range(start, end+1):
     if is_circular_prime(n):
         circular_primes.append(n)
 print(*circular_primes)
+
+# Correct Answer:
+def is_prime(n):
+  if n < 2:     
+    return False
+  for i in range(2, int(n**0.5)+1):      
+    if n % i == 0:   
+      return False  
+  return True
+  
+def rotate(n):
+  s = str(n)
+  return s[1:]+s[0]
+
+def is_circular_prime(n):  
+  s = str(n)
+  for i in range(len(s)):   
+    if not is_prime(int(s)):
+      return False     
+    s = str(rotate(s))
+  return True
+
+start, end = map(int, input().split())
+circular_primes = []
+for n in range(start, end+1): 
+  if is_circular_prime(n):
+    circular_primes.append(n)
+print(*circular_primes)
